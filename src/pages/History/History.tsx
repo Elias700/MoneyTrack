e3a14cfb68150068862a624e4da720c0d723d07b
Elias700 <elias_ribeiro07@outlook.com.br>
2025-12-28 08:20:48 -0300

    feat: adjust header layout and history alignment

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,17 +1,27 @@
-
 import { InputHistoryMonth } from "../../components/UI/Inputs/Inputs";
 import { InputHistoryYear } from "../../components/UI/Inputs/Inputs";
+import { RiDownload2Line } from "react-icons/ri";
 
 const History = () => {
     return (
-        <main className="flex justify-center bg-(--color-primary-50) p-10">
-            <h1 className="text-5xl font-bold text-(--color-primary-300)">Histórico</h1>
-            <section>
-                <form action="" className="flex gap-25 mt-35 ml-40 max-w-md">
+        // Alterado para flex-col para os itens ficarem um abaixo do outro, e w-full para ocupar tudo
+        <main className="flex flex-col w-full bg-(--color-primary-50) p-10">
+            
+            {/* Esta section agora ocupa a largura total e joga os itens para as pontas */}
+            <section className="flex justify-between items-center w-full">
+                <h1 className="text-5xl font-bold text-(--color-primary-300)">Histórico</h1>
+                <button className="cursor-pointer hover:opacity-80 transition-opacity">
+                    <RiDownload2Line size={50} className="text-[var(--color-primary-300)]"/>
+                </button>
+            </section>
+
+            <section className="ml-70">
+                <form action="" className="flex gap-25 mt-35 max-w-md">
                     <InputHistoryMonth />
                     <InputHistoryYear />
                 </form>
-                <div className="flex justify-center w-full mt-70 ml-80 text-(--color-primary-500)">
+                
+                <div className="mt-60 ml-100 text-(--color-primary-500)">
                     <p className="text-3xl text-(--color-primary-300) opacity-70">Pesquise seu histórico</p>
                 </div>
             </section>
@@ -20,4 +30,3 @@ const History = () => {
 };
 
 export default History;
-

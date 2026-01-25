3fb572ff10fd6d85627d248dd463c15f59b9da26
Elias700 <elias_ribeiro07@outlook.com.br>
2026-01-25 15:06:15 -0300

    feat (NotificationSettings): code organization

--- src/pages/Settings/NotificationSettings.tsx
+++ src/pages/Settings/NotificationSettings.tsx
@@ -4,9 +4,16 @@ import { InputToggle } from "../../components/UI/Inputs";
 
 export const NotificationSettings = () => {
   return (
-    <main className="max-w-5xl mx-auto px-4">
-      
-      <div className="flex items-center gap-2 pb-6">
+    <main>
+
+      <div
+        className="
+            flex 
+            items-center 
+            gap-2 
+            pb-6
+          "
+      >
         <FaArrowLeft className="text-(--color-primary-300)" />
         <a
           href="/settings"
@@ -16,7 +23,14 @@ export const NotificationSettings = () => {
         </a>
       </div>
 
-      <h1 className="text-5xl font-bold text-(--color-primary-300) mb-4">
+      <h1
+        className="
+            text-5xl 
+            font-bold 
+            text-(--color-primary-300) 
+            mb-4
+          "
+      >
         Notificações
       </h1>
 
@@ -25,13 +39,43 @@ export const NotificationSettings = () => {
         sobre suas atividades financeiras.
       </p>
 
-      <section className="p-6 space-y-5">
-       
-        <div className="flex items-center justify-between bg-(--color-neutral-200) rounded-lg p-5">
-          <div className="flex gap-3 max-w-xl">
+      <section
+        className="  
+          p-6 
+          space-y-5
+          w-full
+          max-w-full
+          lg:max-w-3xl
+          xl:max-w-4xl
+          mx-auto
+        "
+      >
+
+        <div
+          className="
+              flex 
+              items-center 
+              justify-between 
+              bg-(--color-neutral-200) 
+              rounded-lg 
+              p-5
+            "
+        >
+          <div 
+            className="
+                flex 
+                gap-3
+                max-w-xl
+              "
+            >
             <MdEmail className="text-(--color-primary-300) text-xl mt-1" />
             <div>
-              <h2 className="font-semibold text-(--color-primary-300)">
+              <h2 
+                className="
+                    font-semibold 
+                    text-(--color-primary-300)
+                  "
+                >
                 Notificações por E-mail
               </h2>
               <p className="text-sm text-(--color-desabled)">
@@ -45,8 +89,23 @@ export const NotificationSettings = () => {
           <InputToggle />
         </div>
 
-        <div className="flex items-center justify-between bg-(--color-neutral-200) rounded-lg p-5">
-          <div className="flex gap-3 max-w-xl">
+        <div 
+          className="
+              flex 
+              items-center 
+              justify-between 
+              bg-(--color-neutral-200) 
+              rounded-lg 
+              p-5
+            "
+          >
+          <div 
+            className="
+                flex 
+                gap-3 
+                max-w-xl
+              "
+            >
             <MdNotifications className="text-(--color-primary-300) text-xl mt-1" />
             <div>
               <h2 className="font-semibold text-(--color-primary-300)">
@@ -62,7 +121,16 @@ export const NotificationSettings = () => {
           <InputToggle />
         </div>
 
-        <div className="flex items-center justify-between bg-(--color-neutral-200) rounded-lg p-5">
+        <div 
+          className="
+              flex 
+              items-center 
+              justify-between 
+              bg-(--color-neutral-200) 
+              rounded-lg 
+              p-5
+            "
+          >
           <div className="flex gap-3 max-w-xl">
             <MdCalendarMonth className="text-(--color-primary-300) text-xl mt-1" />
             <div>
